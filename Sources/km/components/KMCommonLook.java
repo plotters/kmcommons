40dbdf16d7dd4a3db1e827351307297da17edca6
hugith <hugith@aec456e2-7d2d-11de-adf7-37ed0412c1db>
2009-08-17 09:26:06 +0000

    Added prettiness

---
+++ Sources/km/components/KMCommonLook.java
@@ -0,0 +1,18 @@
+package km.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.extensions.components.ERXComponent;
+
+/**
+ * Look for our components.
+ * 
+ * @author Hugi Thordarson
+ */
+
+public class KMCommonLook extends ERXComponent {
+
+	public KMCommonLook( WOContext context ) {
+		super( context );
+	}
+}
