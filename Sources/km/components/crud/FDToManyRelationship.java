2186962f8b0ef82cc8d59e07cf30bd8434b7db4d
hugith@gmail.com <hugith@gmail.com@aec456e2-7d2d-11de-adf7-37ed0412c1db>
2010-01-23 15:56:28 +0000

    Added To-Many relationship editing

--- Sources/km/components/crud/FDToManyRelationship.java
+++ Sources/km/components/crud/FDToManyRelationship.java
@@ -2,17 +2,80 @@ package km.components.crud;
 
 import km.components.KMComponent;
 
-import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.*;
+import com.webobjects.eoaccess.EOUtilities;
+import com.webobjects.eocontrol.*;
+import com.webobjects.foundation.*;
 
 /**
- * For editing a to-many relationship.
+ * Inspects a to-many relationship, allowing editing, addition and removal of objects.
  * 
- * @author Hugi Thordarson
+ * @author Hugi Þórðarson
  */
 
 public class FDToManyRelationship extends KMComponent {
 
+	private static final String SELECTED_OBJECT = "selectedObject";
+	public String entityName;
+	public String displayKey;
+	public String fieldName;
+	public String inspectorPageName;
+	public EOEnterpriseObject record;
+
+	public EOEnterpriseObject currentObjectInPopUp;
+	public EOEnterpriseObject selectedObjectInPopUp;
+
+	public EOEnterpriseObject currentObject;
+	public int tableIndex;
+
 	public FDToManyRelationship( WOContext context ) {
 		super( context );
+	}
+
+	public NSArray<EOEnterpriseObject> allObjects() {
+		EOSortOrdering s = new EOSortOrdering( displayKey, EOSortOrdering.CompareAscending );
+		NSMutableArray<EOEnterpriseObject> a = EOUtilities.objectsForEntityNamed( ec(), entityName ).mutableClone();
+		a.removeObjectsInArray( selectedObjects() );
+		return EOSortOrdering.sortedArrayUsingKeyOrderArray( a, new NSArray<EOSortOrdering>( s ) );
+	}
+
+	public Object popUpDisplayString() {
+		return currentObjectInPopUp.valueForKeyPath( displayKey );
+	}
+
+	public Object displayString() {
+		return currentObject.valueForKeyPath( displayKey );
+	}
+
+	public NSArray<EOEnterpriseObject> selectedObjects() {
+		EOSortOrdering s = new EOSortOrdering( displayKey, EOSortOrdering.CompareAscending );
+		NSArray<EOEnterpriseObject> a = (NSArray<EOEnterpriseObject>)record.valueForKeyPath( fieldName );
+		return EOSortOrdering.sortedArrayUsingKeyOrderArray( a, new NSArray<EOSortOrdering>( s ) );
+	}
+
+	public WOActionResults removeObject() {
+		record.removeObjectFromBothSidesOfRelationshipWithKey( currentObject, fieldName );
+		ec().saveChanges();
+		return context().page();
+	}
+
+	public WOActionResults addObject() {
+		record.addObjectToBothSidesOfRelationshipWithKey( selectedObjectInPopUp, fieldName );
+		ec().saveChanges();
+		return context().page();
+	}
+
+	public String bgColor() {
+		return (tableIndex % 2 == 0) ? "#cccccc" : null;
+	}
+
+	public boolean isDisabled() {
+		return inspectorPageName == null;
+	}
+
+	public WOActionResults selectObject() {
+		WOComponent nextPage = pageWithName( inspectorPageName );
+		nextPage.takeValueForKey( currentObject, SELECTED_OBJECT );
+		return nextPage;
 	}
 }
